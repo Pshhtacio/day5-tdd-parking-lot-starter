59934876a2e15cdfa95171b2d4b2ec0a3bbf62a5
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-18 15:28:19 +0800

    test: should_return_a_nothing__when_fetchParkedCar_given_wrong_parkingTicket

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -3,8 +3,6 @@ package com.parkinglot;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ParkingLotTest {
     ParkingLot parkingLot = new ParkingLot();
     Car car = new Car(0);
@@ -26,7 +24,32 @@ class ParkingLotTest {
         Car fetchedCar = parkingLot.fetchCar(parkingTicket);
         //Then
         Assertions.assertFalse(fetchedCar.isParked());
-        Assertions.assertTrue(fetchedCar.IsFetched());
+        Assertions.assertTrue(fetchedCar.isFetched());
+    }
+
+    @Test
+    void should_return_a_parkedCar_for_each_parkingTicket_when_fetchParkedCar_given_two_parkedCar_and_two_parkingTickets() {
+        //Given
+        Car car2 = new Car(1);
+        ParkingTicket parkingTicket = parkingLot.parkCar(car);
+        ParkingTicket parkingTicket2 = parkingLot.parkCar(car2);
+        //When
+        Car fetchedCar = parkingLot.fetchCar(parkingTicket);
+        Car fetchedCar2 = parkingLot.fetchCar(parkingTicket2);
+        //Then
+        Assertions.assertFalse(fetchedCar.isParked());
+        Assertions.assertTrue(fetchedCar.isFetched());
+        Assertions.assertFalse(fetchedCar2.isParked());
+        Assertions.assertTrue(fetchedCar2.isFetched());
+    }
+    @Test
+    void should_return_a_nothing__when_fetchParkedCar_given_wrong_parkingTicket() {
+        //Given
+        ParkingTicket wrongParkingTicket = new ParkingTicket(car);
+        //When
+        Car fetchedCar = parkingLot.fetchCar(wrongParkingTicket);
+        //Then
+        Assertions.assertNull(fetchedCar);
     }
 
 }
