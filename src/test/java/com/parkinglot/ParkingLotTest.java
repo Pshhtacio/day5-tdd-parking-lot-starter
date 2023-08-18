946e7de8017b2b85467ec138d5f70dc545a532c1
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-18 15:12:34 +0800

    test: Add test should_return_a_parkedCar_when_fetchParkedCar_given_a_parkedCar_and_a_parkingTicket

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -6,16 +6,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingLotTest {
+    ParkingLot parkingLot = new ParkingLot();
+    Car car = new Car(0);
+
     @Test
     void should_return_a_parking_ticket_when_parkCar_given_a_car_and_a_parkingLot() {
-        //Given
-        ParkingLot parkingLot = new ParkingLot();
-        Car car = new Car(0);
         //When
         ParkingTicket parkingTicket = parkingLot.parkCar(car);
         //Then
-        Assertions.assertEquals(true, parkingTicket.isValid);
-        Assertions.assertEquals(true, car.isParked());
+        Assertions.assertTrue(parkingTicket.isValid);
+        Assertions.assertTrue(car.isParked());
+    }
+
+    @Test
+    void should_return_a_parkedCar_when_fetchParkedCar_given_a_parkedCar_and_a_parkingTicket() {
+        //Given
+        ParkingTicket parkingTicket = parkingLot.parkCar(car);
+        //When
+        Car fetchedCar = parkingLot.fetchCar(parkingTicket);
+        //Then
+        Assertions.assertFalse(fetchedCar.isParked());
+        Assertions.assertTrue(fetchedCar.IsFetched());
     }
 
 }
