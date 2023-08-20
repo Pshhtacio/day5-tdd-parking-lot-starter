b13516be18b57a357809b00d5caae469410fa874
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-20 18:18:31 +0800

    test: refactor test, addedBeforeEach annotation to set up common objects, removed comments

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -3,73 +3,66 @@ package com.parkinglot;
 import com.parkinglot.exception.FullCapacityException;
 import com.parkinglot.exception.UnrecognizedTicketException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ParkingLotTest {
-    ParkingLot parkingLot = new ParkingLot();
-    Car car = new Car();
+
+    private ParkingLot parkingLot;
+    private Car car;
+
+    @BeforeEach
+    void set_up() {
+        parkingLot = new ParkingLot();
+        car = new Car();
+    }
 
     @Test
-    void should_return_a_parking_ticket_when_parkCar_given_a_car_and_a_parkingLot() {
-        //When
+    void should_return_parking_ticket_when_park_car_given_car_and_parking_lot() {
         ParkingTicket parkingTicket = parkingLot.parkCar(car);
-        //Then
         Assertions.assertNotNull(parkingTicket);
     }
 
     @Test
-    void should_return_a_parkedCar_when_fetchCar_given_a_parkedCar_and_a_parkingTicket() {
-        //Given
+    void should_return_parked_car_when_fetch_car_given_parked_car_and_parking_ticket() {
         ParkingTicket parkingTicket = parkingLot.parkCar(car);
-        //When
         Car fetchedCar = parkingLot.fetchCar(parkingTicket);
-        //Then
         Assertions.assertNotNull(fetchedCar);
     }
 
     @Test
-    void should_return_a_parkedCar_for_each_parkingTicket_when_fetchCar_given_two_parkedCar_and_two_parkingTickets() {
-        //Given
+    void should_return_parked_car_for_each_parking_ticket_when_fetch_car_given_two_parked_cars_and_two_parking_tickets() {
         Car car2 = new Car();
         ParkingTicket parkingTicket = parkingLot.parkCar(car);
         ParkingTicket parkingTicket2 = parkingLot.parkCar(car2);
-        //When
         Car fetchedCar = parkingLot.fetchCar(parkingTicket);
         Car fetchedCar2 = parkingLot.fetchCar(parkingTicket2);
-        //Then
         Assertions.assertNotNull(fetchedCar);
         Assertions.assertNotNull(fetchedCar2);
     }
 
     @Test
-    void should_return_nothing_and_an_error_message_when_fetchCar_given_wrong_parkingTicket() {
-        //Given
+    void should_return_nothing_and_an_error_message_when_fetch_car_given_wrong_parking_ticket() {
         ParkingTicket wrongParkingTicket = new ParkingTicket(car);
-        //When & Then
         Assertions.assertThrows(UnrecognizedTicketException.class, () -> {
             parkingLot.fetchCar(wrongParkingTicket);
         });
     }
 
     @Test
-    void should_return_nothing_and_an_error_message_when_fetchCar_given_used_parkingTicket() {
-        //Given
+    void should_return_nothing_and_an_error_message_when_fetch_car_given_used_parking_ticket() {
         ParkingTicket parkingTicket = parkingLot.parkCar(car);
-        //When
         Car fetchedCar = parkingLot.fetchCar(parkingTicket);
-        //Then
         Assertions.assertThrows(UnrecognizedTicketException.class, () -> {
             parkingLot.fetchCar(parkingTicket);
         });
     }
 
     @Test
-    void should_return_nothing_and_an_error_message_when_parkCar_given_no_parkingLot_available() {
-        //Given
+    void should_return_nothing_and_an_error_message_when_park_car_given_no_parking_lot_available() {
         ParkingLot newParkingLot = new ParkingLot(1);
         Car car2 = new Car();
         ParkingTicket parkingTicket = newParkingLot.parkCar(car);
-        //When and Then
         Assertions.assertThrows(FullCapacityException.class, () -> {
             newParkingLot.parkCar(car2);
         });
