8131dc1f905fae42314f2580ac982625a6bbdd68
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-20 13:00:25 +0800

    refactor: refactor UnrecognizedTicketException added period on message

--- src/main/java/com/parkinglot/exception/UnrecognizedTicketException.java
+++ src/main/java/com/parkinglot/exception/UnrecognizedTicketException.java
@@ -2,6 +2,6 @@ package com.parkinglot.exception;
 
 public class UnrecognizedTicketException extends RuntimeException{
     public UnrecognizedTicketException(){
-        super("Unrecognized parking ticket");
+        super("Unrecognized parking ticket.");
     }
 }
