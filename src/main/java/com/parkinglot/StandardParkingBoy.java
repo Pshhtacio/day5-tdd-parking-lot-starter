9704445b9861315dbc5ce3d54f0b88a88df704d7
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-20 19:23:10 +0800

    feat: extends ParkingBoy class and override methods

--- src/main/java/com/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -5,22 +5,28 @@ import com.parkinglot.exception.UnrecognizedTicketException;
 
 import java.util.List;
 
-public class StandardParkingBoy {
+public class StandardParkingBoy extends ParkingBoy {
     private List<ParkingLot> parkingLots;
 
     public StandardParkingBoy(List<ParkingLot> parkingLots) {
-        this.parkingLots = parkingLots;
+        super(parkingLots);
+        this.parkingLots = super.getParkingLots();
     }
 
+    @Override
     public ParkingTicket parkCar(Car car) {
-        for (ParkingLot parkingLot : parkingLots) {
-            if (parkingLot.hasAvailableSpace(parkingLot)) {
-                return parkingLot.parkCar(car);
-            }
-        }
-        throw new FullCapacityException();
+        ParkingLot availableParkingLot = getAvailableParkingLot();
+        return availableParkingLot.parkCar(car);
     }
 
+    private ParkingLot getAvailableParkingLot() {
+        return parkingLots.stream()
+                .filter(parkingLot -> parkingLot.availableSpace(parkingLot) != 0)
+                .findFirst()
+                .orElseThrow(FullCapacityException::new);
+    }
+
+    @Override
     public Car fetchCar(ParkingTicket parkingTicket) {
         for (ParkingLot parkingLot : parkingLots) {
             Car car = parkingLot.fetchCar(parkingTicket);
